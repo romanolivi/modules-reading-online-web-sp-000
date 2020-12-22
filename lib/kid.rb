e2c873f078775920ac2a279dfaaf3e659c2b8893
Romano Livi <romano_livi@outlook.com>
2020-12-22 21:24:52 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,5 +2,14 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
+ include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+ 
   extend MetaDancing
 end
