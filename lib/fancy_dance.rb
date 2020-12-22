c8f198d59680b4c9d2647c30df0069e6fe0effde
Romano Livi <romano_livi@outlook.com>
2020-12-22 21:47:14 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -14,7 +14,7 @@ module FancyDance
     end
     
     def take_a_bow
-      "Thank you, thanky you. It was a pleasure to dance for you all."
+      "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
     
