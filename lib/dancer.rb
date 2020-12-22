23553e74ce210add30136ab825005f089d7fba01
Romano Livi <romano_livi@outlook.com>
2020-12-22 21:47:50 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,9 +3,13 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer 
-  
-  
-  
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
   
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
